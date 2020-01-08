36717b7ec2a47f4eb0c7ea3af47c80418d6aeb51
Dampen59 <Dampen59@free.fr>
2020-01-08 09:38:39 +0100

    Fix. add Permission entity

---
+++ src/entity/Permission.ts
@@ -0,0 +1,21 @@
+import {Entity, PrimaryGeneratedColumn, Column, ManyToMany} from "typeorm";
+import { IsUUID, IsAlpha } from "class-validator";
+import { Role } from "./Role";
+
+@Entity()
+export class Permission {
+
+    @IsUUID()
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    operation: string;
+
+    @Column()
+    description: string;
+
+    @ManyToMany(type => Role, role => role.permissions, { eager: true })
+    roles: Role[];
+
+}
